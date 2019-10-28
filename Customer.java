7b8521f86667da553fe1a6dd59a7d326b64cffc2
siyaya00 <41107015+siyaya00@users.noreply.github.com>
2019-10-28 14:30:25 +0200

    Add files via upload

---
+++ Customer.java
@@ -0,0 +1,42 @@
+package model;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import model.CustomerEntity;
+
+@ManagedBean(name = "customer")
+@SessionScoped
+
+public class Customer {
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+//	public CustomerEntity getEntity(String msg)
+//	{
+//		CustomerEntity customerEntity = new CustomerEntity();
+//		customerEntity.setName(msg);
+//		return customerEntity;
+//	}
+	
+	public CustomerEntity getEntity()
+	{
+		CustomerEntity customerEntity = new CustomerEntity();
+		customerEntity.setName(name);
+		return customerEntity;
+	}
+	
+	public CustomerEntity submitMessage(String msg)
+	{
+		CustomerEntity customerEntity = new CustomerEntity();
+		customerEntity.setName(name);
+		return customerEntity;
+	}
+}
